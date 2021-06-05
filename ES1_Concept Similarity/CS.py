6826ddc077fa40323f6b772b45bc13998ab6776b
Giacomo <giacomo.chappo@hotmail.it>
2021-06-05 15:39:27 +0200

    wupalmer 2

--- ES1_Concept Similarity/CS.py
+++ ES1_Concept Similarity/CS.py
@@ -2,38 +2,55 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 import nltk
+import numpy as np
 from nltk.corpus import wordnet as wn
-
-
-# %%
-term = "board"
-
-for ss in wn.synsets(term):
-    print(ss.name(), ss.lemma_names())
-    print(f'def: {ss.definition()}')
-    print(f'examples: {ss.examples()}')
-    print("-----hyponyms-----")
-    for hyp in ss.hyponyms():
-        print(f'hyp: {str(hyp)}')
-    print("-----hypernyms-----")
-    for hyper in ss.hypernyms(): 
-        print(f'hyper: {str(hyper)}')
-    print("############\n")
-
+import csv
+from tabulate import tabulate
 
 # %%
-print(wn.synset("board.n.02").part_meronyms())
-print(wn.synset("cat.n.01").part_meronyms())
+with open('WordSim353.csv') as csv_file:
+    matrix = []
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        matrix.append(row)
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+    matrix = np.array(matrix)
+    print(tabulate(matrix[:27,:], headers='firstrow', showindex='always', tablefmt='fancy_grid'))
+    print(matrix[1])
+
+# %%
+# Misure di similarità
+
+# Wu & Palmer
+def Wu_Palmer(term1, term2):
+    sim = 0
+    for s1 in wn.synsets(term1):
+        for s2 in wn.synsets(term2):
+            lcs = s1.lowest_common_hypernyms(s2)
+            for common_ancestor in lcs:
+                nom = 2 * lcs[0].min_depth()
+                den = s1.min_depth() + s2.min_depth()
+                sim = max(sim, nom/den)
+    return sim
+
+# Shortest path
+
+
+# Leakcock & Chodorowee
+
+# %%
+# Indici di correlazione
 
 
 # %%
 s1 = wn.synset("cat.n.01")
-s2 = wn.synset("dog.n.01")
-
-print(f'def({s1}): {s1.definition()}\n')
-print(f'def({s2}): {s2.definition()}\n')
-
+s2 = wn.synset("tiger.n.02")
+score = Wu_Palmer("cat","tiger")
+print(score)
 print(f'WUP_sim({s1},{s2}): {s1.wup_similarity(s2)}')
-print(f'LCH_sim({s1},{s2}): {s1.lch_similarity(s2)}')
-print(f'PTH_sim({s1},{s2}): {s1.path_similarity(s2)}')
+
 # %%
