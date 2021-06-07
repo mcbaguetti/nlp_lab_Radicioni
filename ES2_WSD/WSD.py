8fb03d967a2853c1b79a6051ab70050964f16bb9
Giacomo <giacomo.chappo@hotmail.it>
2021-06-07 17:42:12 +0200

    estrazione dati da xml e scheletro funzione di Lesk

--- ES2_WSD/WSD.py
+++ ES2_WSD/WSD.py
@@ -4,12 +4,63 @@ from nltk.corpus import semcor
 import random
 
 # %%
-sent = semcor.xml('brown1/tagfiles/br-a01.xml').findall('context/p/s')[0]
-for wordform in sent.getchildren():
-    print(wordform.text, end=' ')
-    for key in sorted(wordform.keys()):
-        print(key + '=' + wordform.get(key), end=' ')
-    print()
+def sc2ss(sensekey):
+    """
+    Look up a synset given the information from SemCor
+    """
+    # TODO
+    # if wnsn != 0:
+    #     return wn.lemma_from_key(sensekey).synset()
+    # else:
+    #     pass 
+    return wn.lemma_from_key(sensekey).synset()
+
+
+# %%
+sentences = semcor.xml('brown1/tagfiles/br-a01.xml').findall('context/p/s')
+sent = sentences[0]
+
+# %%
+def print_info(ss):
+    print(ss.name(), ss.lemma_names())
+    print(f'def: {ss.definition()}')
+    print(f'examples: {ss.examples()}')
+    print("-----hyponyms-----")
+    for hyp in ss.hyponyms():
+        print(f'hyp: {str(hyp)}')
+    print("-----hypernyms-----")
+    for hyper in ss.hypernyms(): 
+        print(f'hyper: {str(hyper)}')
+    print("############\n")
+
+# %%
+def get_words(sentence):
+    words = []
+    for word in sentence:
+        #print(word.tag, word.attrib["cmd"])
+        if word.tag == "wf" and word.attrib["cmd"] == "done":
+            if ("ot" not in word.attrib.keys()):
+                lemma = word.attrib["lemma"]
+                words.append(lemma)
+                print('\033[1m' + lemma + '\033[0m')
+                lexsn = word.attrib["lexsn"]
+                ss = sc2ss(lemma + '%' + lexsn)
+                print_info(ss)
+                print()
+    return words
+
+# %%
+def Lesk(word, sentence):
+    best_sense = "a" # TODO
+    max_overlap = 0
+    context = get_words(sentence)
+    for ss in wn.synsets(word):
+        signature = [] # TODO
+        overlap = compute_overlap(signature, context)
+        if overlap > max_overlap:
+            max_overlap = overlap
+            best_sense = ss
+    return best_sense
 
 # %%
 semcor.words()
@@ -20,7 +71,7 @@ semcor.chunks()
 semcor.sents()
 # %%
 c_sents = semcor.chunk_sents()
-c_sents[50]
+c_sents[9]
 
 # %%
 random_list = []
--- ES2_WSD/prove.py
+++ ES2_WSD/prove.py
@@ -14,29 +14,56 @@ for ss in wn.synsets("proportionate"):
     for hyper in ss.hypernyms(): 
         print(f'hyper: {str(hyper)}')
     print("############\n")
+
+# %%
+def print_info(ss):
+    print(ss.name(), ss.lemma_names())
+    print(f'def: {ss.definition()}')
+    print(f'examples: {ss.examples()}')
+    print("-----hyponyms-----")
+    for hyp in ss.hyponyms():
+        print(f'hyp: {str(hyp)}')
+    print("-----hypernyms-----")
+    for hyper in ss.hypernyms(): 
+        print(f'hyper: {str(hyper)}')
+    print("############\n")
 # %%
 def sc2ss(sensekey):
     """
     Look up a synset given the information from SemCor
     """
-    if wnsn != 0:
-        return wn.lemma_from_key(sensekey).synset()
-    else:
-        pass # TODO
- 
+    # TODO
+    # if wnsn != 0:
+    #     return wn.lemma_from_key(sensekey).synset()
+    # else:
+    #     pass 
+    return wn.lemma_from_key(sensekey).synset()
+
+
+# %%
 s1 = sc2ss('live%2:42:06::')
 s2 = sc2ss('overall%5:00:00:gross:00')
-ss = s2
-print(ss.name(), ss.lemma_names())
-print(f'def: {ss.definition()}')
-print(f'examples: {ss.examples()}')
-print("-----hyponyms-----")
-for hyp in ss.hyponyms():
-    print(f'hyp: {str(hyp)}')
-print("-----hypernyms-----")
-for hyper in ss.hypernyms(): 
-    print(f'hyper: {str(hyper)}')
-print("############\n")
+s3 = sc2ss('primary_election%1:04:00::')
+ss = s3
+# %%
+sentences = semcor.xml('brown1/tagfiles/br-a01.xml').findall('context/p/s')
+sent = sentences[0]
+for word in sent:
+    #print(word.tag, word.attrib["cmd"])
+    if word.tag == "wf" and word.attrib["cmd"] == "done":
+        if ("ot" not in word.attrib.keys()):
+            lemma = word.attrib["lemma"]
+            print('\033[1m' + lemma + '\033[0m')
+            lexsn = word.attrib["lexsn"]
+            ss = sc2ss(lemma + '%' + lexsn)
+            print_info(ss)
+    print()
+
+# %%
+a = []
+a.append(1)
+a.append(2)
+a
 # %% [markdown]
 http://moin.delph-in.net/wiki/SemCor
 https://www.nltk.org/api/nltk.corpus.reader.html#nltk.corpus.reader.semcor.SemcorCorpusReader
