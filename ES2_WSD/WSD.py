1f448a2bd60eec770405c506c9d59fdd306d490e
Giacomo <giacomo.chappo@hotmail.it>
2021-06-08 21:09:18 +0200

    completamento aloritmo

--- ES2_WSD/WSD.py
+++ ES2_WSD/WSD.py
@@ -1,63 +1,70 @@
 # %%
 import nltk
+from nltk.corpus import wordnet as wn
 from nltk.corpus import semcor
-import random
 import re
+import os
+import random
 
+random.seed(1997) # TODO: togliere
+# TODO: sostituire con lettura file
+stop_words = ["a","able","about","above","abst","accordance","according","accordingly","across","act","actually","added","adj","adopted","affected","affecting","affects","after","afterwards","again","against","ah","all","almost","alone","along","already","also","although","always","am","among","amongst","an","and","announce","another","any","anybody","anyhow","anymore","anyone","anything","anyway","anyways","anywhere","apparently","approximately","are","aren","arent","arise","around","as","aside","ask","asking","at","auth","available","away","awfully","b","back","be","became","because","become","becomes","becoming","been","before","beforehand","begin","beginning","beginnings","begins","behind","being","believe","below","beside","besides","between","beyond","biol","both","brief","briefly","but","by","c","ca","came","can","cannot","can't","cause","causes","certain","certainly","co","com","come","comes","contain","containing","contains","could","couldnt","d","date","did","didn't","different","do","does","doesn't","doing","done","don't","down","downwards","due","during","e","each","ed","edu","effect","eg","eight","eighty","either","else","elsewhere","end","ending","enough","especially","et","et-al","etc","even","ever","every","everybody","everyone","everything","everywhere","ex","except","f","far","few","ff","fifth","first","five","fix","followed","following","follows","for","former","formerly","forth","found","four","from","further","furthermore","g","gave","get","gets","getting","give","given","gives","giving","go","goes","gone","got","gotten","h","had","happens","hardly","has","hasn't","have","haven't","having","he","hed","hence","her","here","hereafter","hereby","herein","heres","hereupon","hers","herself","hes","hi","hid","him","himself","his","hither","home","how","howbeit","however","hundred","i","id","ie","if","i'll","im","immediate","immediately","importance","important","in","inc","indeed","index","information","instead","into","invention","inward","is","isn't","it","itd","it'll","its","itself","i've","j","just","k","keep","keeps","kept","keys","kg","km","know","known","knows","l","largely","last","lately","later","latter","latterly","least","less","lest","let","lets","like","liked","likely","line","little","'ll","look","looking","looks","ltd","m","made","mainly","make","makes","many","may","maybe","me","mean","means","meantime","meanwhile","merely","mg","might","million","miss","ml","more","moreover","most","mostly","mr","mrs","much","mug","must","my","myself","n","na","name","namely","nay","nd","near","nearly","necessarily","necessary","need","needs","neither","never","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","nor","normally","nos","not","noted","nothing","now","nowhere","o","obtain","obtained","obviously","of","off","often","oh","ok","okay","old","omitted","on","once","one","ones","only","onto","or","ord","other","others","otherwise","ought","our","ours","ourselves","out","outside","over","overall","owing","own","p","page","pages","part","particular","particularly","past","per","perhaps","placed","please","plus","poorly","possible","possibly","potentially","pp","predominantly","present","previously","primarily","probably","promptly","proud","provides","put","q","que","quickly","quite","qv","r","ran","rather","rd","re","readily","really","recent","recently","ref","refs","regarding","regardless","regards","related","relatively","research","respectively","resulted","resulting","results","right","run","s","said","same","saw","say","saying","says","sec","section","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sent","seven","several","shall","she","shed","she'll","shes","should","shouldn't","show","showed","shown","showns","shows","significant","significantly","similar","similarly","since","six","slightly","so","some","somebody","somehow","someone","somethan","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specifically","specified","specify","specifying","state","states","still","stop","strongly","sub","substantially","successfully","such","sufficiently","suggest","sup","sure","t","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","thered","therefore","therein","there'll","thereof","therere","theres","thereto","thereupon","there've","these","they","theyd","they'll","theyre","they've","think","this","those","thou","though","thoughh","thousand","throug","through","throughout","thru","thus","til","tip","to","together","too","took","toward","towards","tried","tries","truly","try","trying","ts","twice","two","u","un","under","unfortunately","unless","unlike","unlikely","until","unto","up","upon","ups","us","use","used","useful","usefully","usefulness","uses","using","usually","v","value","various","'ve","very","via","viz","vol","vols","vs","w","want","wants","was","wasn't","way","we","wed","welcome","we'll","went","were","weren't","we've","what","whatever","what'll","whats","when","whence","whenever","where","whereafter","whereas","whereby","wherein","wheres","whereupon","wherever","whether","which","while","whim","whither","who","whod","whoever","whole","who'll","whom","whomever","whos","whose","why","widely","willing","wish","with","within","without","won't","words","world","would","wouldn't","www","x","y","yes","yet","you","youd","you'll","your","youre","yours","yourself","yourselves","you've","z","zero"]
 # %%
 def sc2ss(sensekey):
     """
     Look up a synset given the information from SemCor
     """
-    return wn.lemma_from_key(sensekey).synset()
+    try:
+        return wn.lemma_from_key(sensekey).synset()
+    except: # No synset found for key
+        return None
 
+def get_random_sentence():
+    rand_int = random.randrange(50000)
+    directories = ["brown1", "brown2", "brownv"]
 
-# %%
-sentences = semcor.xml('brown1/tagfiles/br-a01.xml').findall('context/p/s')
-sent = sentences[0]
+    random_index = rand_int % len(directories)
+    local_path = directories[random_index] + '/tagfiles'
+    abs_path = '/home/xps/nltk_data/corpora/semcor/' + local_path
 
-# %%
-def print_info(ss):
-    print(ss.name(), ss.lemma_names())
-    print(f'def: {ss.definition()}')
-    print(f'examples: {ss.examples()}')
-    print("-----hyponyms-----")
-    for hyp in ss.hyponyms():
-        print(f'hyp: {str(hyp)}')
-    print("-----hypernyms-----")
-    for hyper in ss.hypernyms(): 
-        print(f'hyper: {str(hyper)}')
-    print("############\n")
+    random_index = rand_int % len(next(os.walk(abs_path))[2])
+    current_file = next(os.walk(abs_path))[2][random_index]
+    sentences = semcor.xml(local_path+'/'+current_file).findall('context/p/s')
+    sc_sent = sentences[rand_int % len(sentences)]
 
-def get_random_set():
-    random_set = set()
-    while len(random_set) != 50:
-    # random numbers from 0 to len(semcor.chunk_sents())
-        random_set.add(random.randint(0, 37176)) 
-    return random_set
+    return sc_sent
+
+def get_random_word(sentence):
+    found = False
+    iterations = 0
+    result = {}
+    while iterations < len(sentence) and not found:
+        rand_int = random.randrange(50000)
+        word = sentence[rand_int % len(sentence)]
+        if word.tag == "wf" and word.attrib["cmd"] == "done":
+            if ("ot" not in word.attrib.keys()):
+                if word.attrib["wnsn"] != "0":
+                    term = word.text
+                    lemma = word.attrib["lemma"]
+                    lexsn = word.attrib["lexsn"]
+                    ss = sc2ss(lemma + '%' + lexsn)
+                    if ss is not None and lemma not in stop_words:
+                        result["term"] = term
+                        result["lemma"] = lemma
+                        result["sense"] = ss
+                        found = True
+        iterations += 1
+    
+    return result             
 
 # %%
 def get_context(sentence):
-    words = set()
-    # for word in sentence:
-    #     #print(word.tag, word.attrib["cmd"])
-    #     if word.tag == "wf" and word.attrib["cmd"] == "done":
-    #         if ("ot" not in word.attrib.keys()):
-    #             lemma = word.attrib["lemma"]
-    #             words.add(lemma)
-    #             print('\033[1m' + lemma + '\033[0m')
-    #             lexsn = word.attrib["lexsn"]
-    #             wnsn = word.attrib["wnsn"]
-    #             if wnsn != "0":
-    #                 ss = sc2ss(lemma + '%' + lexsn)
-    #                 print_info(ss)
-    #             print()
-    sentence = re.sub(r'[^\w\s]', '', sentence)
-    words = set(sentence.split())
+    words = set(sentence)
+    words = set(map(lambda x:x.lower(),words))
+    words = words.difference(stop_words)
+
     return words
 
-
-# %%
 def get_signature(sense):
     gloss = sense.definition()
     gloss = re.sub(r'[^\w\s]', '', gloss)
@@ -68,30 +75,59 @@ def get_signature(sense):
     for ex in examples:
         ex = re.sub(r'[^\w\s]', '', ex)
         examples_set.update(ex.split())
-    
-    return gloss_set.union(examples_set)
+    signature = gloss_set.union(examples_set)
+    signature = set(map(lambda x:x.lower(),signature))
+    signature = signature.difference(stop_words)
 
-# %%
+    return signature
+
 def compute_overlap(signature, context):
     return len(signature.intersection(context))
 
 # %%
 def Lesk(word, sentence):
-    best_sense = None # CHECK
+    best_sense = wn.synsets(word)[0] # CHECK
     max_overlap = 0
     context = get_context(sentence)
     for ss in wn.synsets(word):
         signature = get_signature(ss)
         overlap = compute_overlap(signature, context)
+        for hyp in ss.hyponyms():
+            signature = get_signature(hyp)
+            overlap += compute_overlap(signature, context)
         if overlap > max_overlap:
             max_overlap = overlap
             best_sense = ss
     return best_sense
 
+# %%
+def get_possible_pair():
+    found = False
+    while not found:
+        sentence = get_random_sentence()
+        word = get_random_word(sentence)
+        if word:
+            found = True
+    return sentence, word
 
 # %%
-word = "bank"
-sentence = "the bank can guarantee deposits will eventually cover future tuition costs because it invests in adjustable-rate mortgage securities."
-ss = Lesk(word, sentence)
-ss
+num_corrects = 0
+num_iterations = 10
+num_phrases = 50
+for i in range(num_iterations):
+    for j in range(num_phrases):
+        sentence, word = get_possible_pair()
+        phrase = []
+        for sent_word in sentence:
+            phrase.append(sent_word.text)
+        lemma = word["lemma"]
+        best = Lesk(lemma, phrase)
+
+        if best == word["sense"]:
+            num_corrects += 1
+
+print(num_corrects)
+print(num_corrects/(num_iterations*num_phrases))
+
+
 # %%
--- ES2_WSD/prove.py
+++ ES2_WSD/prove.py
@@ -3,6 +3,9 @@ import nltk
 import numpy as np
 import re
 from nltk.corpus import wordnet as wn
+from nltk.corpus import semcor
+import os
+import random
 # %%
 for ss in wn.synsets("bank"):
     print(ss.name(), ss.lemma_names())
@@ -46,18 +49,39 @@ s1 = sc2ss('live%2:42:06::')
 s2 = sc2ss('overall%5:00:00:gross:00')
 s3 = sc2ss('primary_election%1:04:00::')
 ss = s3
+
+# %%
+directories = ["brown1", "brown2", "brownv"]
+random_index = random.randrange(50000) % len(directories)
+local_path = directories[random_index] + '/tagfiles'
+abs_path = '/home/xps/nltk_data/corpora/semcor/' + local_path
+print(random_index, local_path, abs_path)
+#print(len(next(os.walk(path))[2]))
+#print((next(os.walk(abs_path))[2]))
+random_index = random.randrange(50000) % len(next(os.walk(abs_path))[2])
+current_file = next(os.walk(abs_path))[2][random_index]
+print(random_index, current_file)
+print(local_path+'/'+current_file)
+# path = '/home/xps/nltk_data/corpora/semcor/brown1'
+# for root, directories, files in os.walk(path):
+# 	for name in files:
+# 		print("file",os.path.join(root, name))
+# 	for name in directories:
+# 		print("dir",os.path.join(root, name))
+
 # %%
 sentences = semcor.xml('brown1/tagfiles/br-a01.xml').findall('context/p/s')
+#sentences = semcor.xml(local_path+'/'+current_file).findall('context/p/s')
 sent = sentences[0]
 for word in sent:
-    #print(word.tag, word.attrib["cmd"])
+    print(word.text)
     if word.tag == "wf" and word.attrib["cmd"] == "done":
         if ("ot" not in word.attrib.keys()):
             lemma = word.attrib["lemma"]
             print('\033[1m' + lemma + '\033[0m')
-            lexsn = word.attrib["lexsn"]
-            ss = sc2ss(lemma + '%' + lexsn)
-            print_info(ss)
+            # lexsn = word.attrib["lexsn"]
+            # ss = sc2ss(lemma + '%' + lexsn)
+            # print_info(ss)
     print()
 
 # %%
@@ -83,4 +107,19 @@ semcor.chunks()
 semcor.sents()
 # %%
 c_sents = semcor.chunk_sents()
-c_sents[9]
+c_sents
+# %%
+try:
+  ss = wn.lemma_from_key('tell%2:32:01::').synset()
+  print(ss)
+except:
+    # No synset found for key
+  print("An exception occurred")
+# %%
+a = set([1,2,3,4,5])
+a.difference([4,5])
+# %%
+word = 'manometer'
+for ss in wn.synsets(word):
+    print_info(ss)
+# %%
