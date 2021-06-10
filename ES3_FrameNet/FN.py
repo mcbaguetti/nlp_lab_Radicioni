b1303d7f2737ddc14f3c718c889788f6e0faf324
Giacomo <giacomo.chappo@hotmail.it>
2021-06-10 20:01:09 +0200

    scarso successo con la terza esercitazione..

--- ES3_FrameNet/FN.py
+++ ES3_FrameNet/FN.py
@@ -1,2 +1,17 @@
 # To add a new cell, type '# %%'
 # %%
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import framenet as fn
+import re
+# %%
+frame_set = {
+    1599:"Proliferating_in_number",
+	320:"Try_defendant",
+	195:"Political_locales",
+	1609:"Sent_items",
+	5:"Causation"
+}
+for key, frame in frame_set.items():
+    print(key,frame)
+# %%
